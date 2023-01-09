64aead87f894a17506e819182adada8eb04ad132
Evgeny <evolenko@inbox.ru>
2023-01-09 21:57:03 +0300

    Added two variants algoritms

---
+++ Programming/Algoritms_in_C#/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+
+int size = 1_000_000;
+int m = 3_000;
+
+int[] array = Enumerable.Range(1, size)
+                        .Select(item => Random.Shared.Next(10))
+                        .ToArray();
+
+// Console.WriteLine($"[{string.Join(", ", array)}]");
+
+Stopwatch sw = new();
+sw.Start();
+
+int max = 0;
+
+for (int j = 0; j < m; j++) max += array[j];
+int t = max;
+
+for (int i = 1; i < array.Length - m; i++)
+{
+    t = t - array[i - 1] + array[i + (m - 1)];
+    if (t > max) max = t;
+}
+
+sw.Stop();
+
+Console.WriteLine($"Time = {sw.ElapsedMilliseconds}");
+Console.WriteLine($"Результат первого алогритма = {max}");
+
+// Плохой вариант
+
+sw.Start();
+
+int max1 = 0;
+int t1;
+
+for (int i = 1; i < array.Length - m; i++)
+{
+    t1 = 0;
+    for (int j = i; j < i + m; j++) t1 += array[j];
+    if (t1 > max1) max1 = t1;
+}
+
+sw.Stop();
+
+Console.WriteLine($"Time = {sw.ElapsedMilliseconds}");
+Console.WriteLine($"Результат второго алогритма = {max1}");
