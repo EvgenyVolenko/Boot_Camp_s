0a45b1efea93800c21301a1ec6e63fe2cb5521f4
Evgeny <evolenko@inbox.ru>
2023-01-10 22:39:42 +0300

    Added BigONotation

---
+++ Programming/Big_O_Notation/Program.cs
@@ -0,0 +1,54 @@
+﻿Console.Clear();
+Console.WriteLine("Введите размер матрицы ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+{
+    for (int j = 1; j <= n; j++)
+    {
+        Console.Write(i * j + "\t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+// Заполнение через половину матрицы, если i + j <= n
+// O(n^2 / 2)
+
+int[, ] matrix = new int [n, n];
+// for (int i = 0; i < n; i++)
+// {
+//     for (int j = 0; j < n; j++)
+//     {
+//         if (i + j <= n)
+//         {
+//             matrix[i, j] = (i + 1) * (j + 1);
+//             matrix[j, i] = (i + 1) * (j + 1);
+//         }
+//         Console.Write((i + 1) * (j + 1) + "\t");
+//     }
+//     Console.WriteLine();
+// }
+// Console.WriteLine();
+
+// Верный вариант
+
+ for (int i = 0; i < n; i++)
+{
+    for (int j = i; j < n; j++)
+    {
+        matrix[i, j] = (i + 1) * (j + 1);
+        matrix[j, i] = (i + 1) * (j + 1);
+        Console.Write((i + 1) * (j + 1) + "\t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(matrix[i , j] + "\t");
+    }
+    Console.WriteLine();
+}
