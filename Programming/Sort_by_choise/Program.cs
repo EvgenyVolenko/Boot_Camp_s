6cd2496082e041f2238a65c97bcdaded28fc26f5
Evgeny <evolenko@inbox.ru>
2023-01-11 22:50:19 +0300

    добавлен простой пример сортировки выбором

--- Programming/Sort_by_choise/Program.cs
+++ Programming/Sort_by_choise/Program.cs
@@ -23,29 +23,50 @@ MIN = 15
 15 > 9
 [-3, 2, 6, 9, 15]
 */
-Console.WriteLine("Введите кол-во элементов массива:");
-int n = Convert.ToInt32(Console.ReadLine());
-// Заполнение массива
-int[] array = new int[n];
-for (int i = 0; i < n; i++)
-{
-    Console.Write("Введите число: ");
-    array[i] = Convert.ToInt32(Console.ReadLine());
-}
-Console.WriteLine();
+// Console.WriteLine("Введите кол-во элементов массива:");
+// int n = Convert.ToInt32(Console.ReadLine());
+// // Заполнение массива
+// int[] array = new int[n];
+// for (int i = 0; i < n; i++)
+// {
+//     Console.Write("Введите число: ");
+//     array[i] = Convert.ToInt32(Console.ReadLine());
+// }
+// Console.WriteLine();
+// Console.WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");
+// // Cортировка
+// for (int i = 0; i < n - 1; i++)
+// {
+//     int MinIndex = i;
+//     for (int j = i + 1; j < n; j++)
+//     {
+//         if (array[j] < array[MinIndex])
+//             MinIndex = j; 
+//     }
+//     int temp;
+//     temp = array[MinIndex];
+//     array[MinIndex] = array[i];
+//     array[i] = temp;
+// }
+// Console.WriteLine("Конечный массив: [" + string.Join(", ", array) + "]");
+
+int[] array = {7, 6, 3, 4, 5, 1, 2, 3};
 Console.WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");
-// Cортировка
-for (int i = 0; i < n - 1; i++)
-{
-    int MinIndex = i;
-    for (int j = i + 1; j < n; j++)
-    {
-        if (array[j] < array[MinIndex])
-            MinIndex = j; 
-    }
-    int temp;
-    temp = array[MinIndex];
-    array[MinIndex] = array[i];
-    array[i] = temp;
-}
+SortSelection(array);
 Console.WriteLine("Конечный массив: [" + string.Join(", ", array) + "]");
+
+void SortSelection(int[] collection)
+{
+    int size = collection.Length;
+    for (int i = 0; i < size - 1; i++)
+    {
+        int pos = i;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (collection[j] < collection[pos]) pos = j;
+        }
+        int temp = collection[i];
+        collection[i] = collection[pos];
+        collection[pos] = temp;
+    }
+}
