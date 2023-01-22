a4640b1617a099be0d30978dcd99e4b79efe22a8
Evgeny <evolenko@inbox.ru>
2023-01-22 16:31:05 +0300

    Добавлен пример работы ООП

--- Programming/ExploringOpenCV/main.py
+++ Programming/ExploringOpenCV/main.py
@@ -1,8 +1,8 @@
 import cv2
 
-img = cv2.imread('test.jpg')
+img = cv2.imread('G:/!!!!!ARC/Evgeny/!!!GB/YaDisk/YandexDisk/test.jpg')
 
-#img = cv2.resize(img, (500, 500))
+# img = cv2.resize(img, (500, 500))
 
 cv2.imshow('Result', img)
 
